b29f4ec9c6f1a069b77d97c0fa8b2dd95a676e26
SPRAVEDLIVO <misha.domanov@mail.ru>
2022-10-20 17:54:04 +0300

    ты не шутер ты шлюха

---
+++ 3.1/3.1_14.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#define rows 3
+#define columns 7
+using namespace std;
+
+
+int main()
+{
+	double A[rows][columns];
+	int count = 0;
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			cout << "A[" << i << "][" << j << "] = "; cin >> A[i][j];
+		}
+	}
+
+	for (int j = 0; j < columns; j++) {
+		if (A[2][j] != (A[0][j] - A[1][j])) {
+			cout << j + 1 << endl;
+			count++;
+		}
+	}
+
+	cout << "count = " << count << endl;
+	system("pause");
+}
+/*
+Результат:
+A[0][0] = 30
+A[0][1] = 29
+A[0][2] = 28
+A[0][3] = 27
+A[0][4] = 26
+A[0][5] = 25
+A[0][6] = 24
+A[1][0] = 10
+A[1][1] = 9
+A[1][2] = 8
+A[1][3] = 7
+A[1][4] = 6
+A[1][5] = 5
+A[1][6] = 4
+A[2][0] = 3
+A[2][1] = 21
+A[2][2] = 21
+A[2][3] = 21
+A[2][4] = 20
+A[2][5] = 74
+A[2][6] = 21
+1
+2
+3
+4
+6
+7
+count = 6
+*/
--- make.py
+++ make.py
@@ -10,10 +10,11 @@ found[1] += ".cpp"
 
 for f in os.listdir(d):
     if os.path.splitext(f)[1] == ".cpp":
-        with open(os.path.join(d, f), encoding="utf_8_sig") as f:
-            if not os.path.exists(found[0]):
-                os.makedirs(found[0])
-            with open(os.path.join(found[0], "_".join(found)), "w", encoding="utf_8_sig") as fw:
-                fw.write(f.read())
+        with open(os.path.join(d, f), encoding="windows-1251") as fr:
+            buff = fr.read()
+        if not os.path.exists(found[0]):
+            os.makedirs(found[0])
+        with open(os.path.join(found[0], "_".join(found)), "w", encoding="utf-8-sig") as fw:
+            fw.write(buff)
 
 print("Work done! Saved another 30 seconds.")
