760df2aad8a3024df9c62336829f68114bdc27f8
SPRAVEDLIVO <misha.domanov@mail.ru>
2022-12-07 21:52:52 +0300

    add labs via bot

---
+++ 6.2/6.2_18.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main() {
+	ifstream fin("input.txt");
+	ofstream fout("output.txt");
+	string s, out = ""; getline(fin, s);
+	string words[100];
+	int st = 0, len = 0, word = 0; bool flag = true;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == ' ') {
+			if (flag) {
+				words[word] = s.substr(st, len);
+				cout << s.substr(st, len) << endl;
+				word++;
+				st = -1;
+				flag = false;
+			}
+		}
+		else {
+			flag = true;
+			if (st == -1) {
+				st = i; len = 1;
+			}
+			else
+				len++;
+		}
+	}
+	if (st != -1) {
+		words[word] = s.substr(st, len);
+		word++;
+	}
+	for (int i = 0; i < word; i++)
+		out += words[i] + ' ';
+	fout << out;
+	return 0;
+}
